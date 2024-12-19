a440e55c67a15c5ef22a1fdfc1df1e0076b4deec
vanwinkleck <vanwinkleck@protonmail.com>
2024-12-19 15:21:41 -0600

    initial commit, already went through first part of the tutorial, need to set up makeFile

---
+++ shell.nix
@@ -0,0 +1,30 @@
+with import <nixpkgs> {};
+
+mkShell {
+  name = "vulkan";
+  packages = [
+    make
+    cmake
+    dotnet-sdk_8
+    glfw
+    glm
+    freetype
+    vulkan-headers
+    vulkan-loader
+    vulkan-validation-layers
+    vulkan-tools        # vulkaninfo
+    shaderc             # GLSL to SPIRV compiler - glslc
+    renderdoc           # Graphics debugger
+    tracy               # Graphics profiler
+    vulkan-tools-lunarg # vkconfig
+  ];
+
+  buildInputs = with pkgs; [
+    glfw
+    freetype
+  ];
+
+  LD_LIBRARY_PATH="${glfw}/lib:${freetype}/lib:${vulkan-loader}/lib:${vulkan-validation-layers}/lib";
+  VULKAN_SDK = "${vulkan-headers}";
+  VK_LAYER_PATH = "${vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+}
