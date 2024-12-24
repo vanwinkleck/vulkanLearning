4bf1d294617a1680597373a729f8c746c27a6b0a
vanwinkleck <vanwinkleck@protonmail.com>
2024-12-23 18:36:28 -0600

    started drawing traingle section

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,41 @@
-#include <vulkan/vulkan_core.h>
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+#include <vulkan/vulkan.h>
 
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
 
-int main() {
-	glfwInit();
+class HelloTriangleApplication {
+	public: 
+		void run() {
+			initVulkan();
+			mainLoop();
+			cleanup();
+		}
 
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	private: 
+		void initVulkan() {
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+		}
 
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+		void mainLoop() {
 
-	std::cout << extensionCount << " extensions supported\n";
+		}
 
-	glm::mat4 matrix;
-	glm::vec4 vec;
-	auto test = matrix * vec;
+		void cleanup() {
 
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-	}
+		}
 
-	glfwDestroyWindow(window);
+	};
 
-	glfwTerminate();
+	int main() {
+		HelloTriangleApplication app;
 
-	return 0;
+		try {
+			app.run();
+		} catch (const std::exception& e) {
+			std::cerr << e.what() << std::endl;
+			return EXIT_FAILURE;
+		}
 
+	return EXIT_SUCCESS;
 }
